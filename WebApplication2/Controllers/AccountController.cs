279b25cfdbddcfe80602396dcc5be234bf53d159
oschdez97 <oschdez97@gmail.com>
2021-02-05 14:03:02 +0100

    init

---
+++ WebApplication2/Controllers/AccountController.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> userManager;
+        private readonly RoleManager<IdentityRole> roleManager;
+        private readonly IConfiguration configuration;
+
+        public AccountController(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager,
+            IConfiguration configuration)
+        {
+            this.userManager = userManager;
+            this.roleManager = roleManager;
+            this.configuration = configuration;
+        }
+
+        [Route("Register")]
+        [HttpPost]
+        public async Task<IActionResult> Register([FromBody] Register model)
+        {
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+            var user = new ApplicationUser() { UserName = model.Email, Email = model.Email };
+            var result = await userManager.CreateAsync(user, model.Password);
+            if (!result.Succeeded) return BadRequest("UserName or Password Invalid");
+            return Ok(user);
+        }
+
+        [Route("Login")]
+        [HttpPost]
+        public async Task<IActionResult> Login([FromBody] Register model)
+        {
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+            var user = await userManager.FindByEmailAsync(model.Email);
+            if (user != null && await userManager.CheckPasswordAsync(user, model.Password))
+            {
+                var roles = await userManager.GetRolesAsync(user);
+                return BuildToken(user); ;
+            }
+            return BadRequest("UserName or Password Invalid");
+        }
+
+        [Route("PutRoleToUser")]
+        [HttpPost]
+        public async Task<IActionResult> PutRoleToUser([FromBody] RoleAssignment model)
+        {
+            var user = await userManager.FindByEmailAsync(model.Email);
+
+            if (await roleManager.FindByNameAsync(model.Role) == null)
+            {
+                await roleManager.CreateAsync(new IdentityRole(model.Role));
+            }
+            var result = await userManager.AddToRoleAsync(user, model.Role);
+            if (!result.Succeeded) return BadRequest();
+            return Ok();
+        }
+
+        [Route("GetClaims")]
+        [HttpGet]
+        public IActionResult GetClaims()
+        {
+            var claims = User.Claims.Select(claim => new { claim.Type, claim.Value }).ToArray();
+            return Ok(claims);
+        }
+
+        private IActionResult BuildToken(ApplicationUser user)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(configuration["SECRET_KEY"]);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.Email, user.Email),
+                    new Claim(ClaimTypes.Name, user.Email),
+                    new Claim(ClaimTypes.Role, "Admin")
+                }),
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return Ok(tokenHandler.WriteToken(token));
+        }
+    }
+}
---
+++ WebApplication2/Models/RoleAssignment.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication2.Models
+{
+    public class RoleAssignment
+    {
+        public string Email { get; set; }
+        public string Role { get; set; }
+    }
+}
